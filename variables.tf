bd1c0854feb63399244a283d94fd32eaaf5c3303
worldvit <ccna.kim@gmail.com>
2026-03-23 13:18:16 +0900

    Add terraform files

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-west-2"
+}
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+}
